cec6cca3fdcb0f1d2164890a68f9f4649f26bfb2
Ryan Koo <ryank21152@isd273.org>
2017-09-21 11:35:21 -0500

    "updated version made by Richik SC"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BilAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BilAutonomous.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 /**
  * Created by ryankoo on 9/19/17.
  */
 
-public class BilAutonomous extends OpModeBil {
+@Autonomous (name = "BilAuto", group = "robot")
+public class BilAutonomous extends BilTeleOp{
 
 
 }
