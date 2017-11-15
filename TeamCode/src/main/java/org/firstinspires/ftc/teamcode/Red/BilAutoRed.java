e6ea1a7db2aae0bef631f4e909023deb085d322a
patricktehrani <ptatvalley@gmail.com>
2017-11-14 19:52:28 -0600

    i've edited something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red/BilAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red/BilAutoRed.java
@@ -70,7 +70,7 @@ public class BilAutoRed extends LinearOpMode {
                     telemetry.update();
                 }
 
-                // Movement ( TIME AND POWER TO BE DETERMINED )
+                // Movement
                 // 1:  go forward
                 movement.directTankDrive(FORWARD_SPEED, -FORWARD_SPEED);
                 runtime.reset();
