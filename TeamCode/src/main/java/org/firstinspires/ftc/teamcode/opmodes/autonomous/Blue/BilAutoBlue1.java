d1d494241d80081e68ab15743500f95c5f256efa
Ryan K <ryank21152@isd273.org>
2017-11-28 09:12:10 -0600

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Blue/BilAutoBlue1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Blue/BilAutoBlue1.java
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode.opmodes.autonomous.Blue;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vvftc.ninevolt.core.hw.Hardware;
+import com.vvftc.ninevolt.core.hw.drivetrain.standard.Movement;
 
+import org.firstinspires.ftc.teamcode.HardwarePushbot;
 import org.firstinspires.ftc.teamcode.opmodes.modes.SafetyZone_LONG;
 
 /**
@@ -12,18 +15,27 @@ import org.firstinspires.ftc.teamcode.opmodes.modes.SafetyZone_LONG;
 @Autonomous(name = "blueLong", group = "robot")
 public class BilAutoBlue1 extends LinearOpMode {
 
+    private Movement movement;
+    private Hardware hardware;
+    private HardwarePushbot robot;
+    private LinearOpMode ctx;
+
     public SafetyZone_LONG safetyZone;
 
     @Override
     public void runOpMode() {
 
-        safetyZone = new SafetyZone_LONG();
+
+        safetyZone = new SafetyZone_LONG(hardware,movement,robot,ctx);
+
         telemetry.addData("Status", "Ready to start");
         telemetry.update();
 
         waitForStart();
 
         sleep(2000);
+        // Makes sure claw doesn't open at init
+        robot.init(hardwareMap);
 
         safetyZone.run(); // move robot to safety zone
 
